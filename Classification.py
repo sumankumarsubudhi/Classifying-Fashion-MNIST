a1ff023e2fce4c3a5429fa808d16a7785333a01b
Suman Kumar Subudhi <sonu61866@gmail.com>
2019-08-10 23:31:10 +0530

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -10,6 +10,7 @@ import helper
 # Define a transform to normalize the data
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
 # Download and load the training data
 trainset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
