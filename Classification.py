9e13b68a59ec6385a695b349fb22b57849bdc81f
Suman Kumar Subudhi <sonu61866@gmail.com>
2019-08-10 23:31:36 +0530

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -44,6 +44,7 @@ class Classifier(nn.Module):
         return x
         
 # Train the network
+
 # Create the network, define the criterion and optimizer
 model = Classifier()
 criterion = nn.NLLLoss()
